1d818dab85b30e209477cfb30a81bcb1717bf32f
André Cruz <andremiguelcruz@msn.com>
2016-01-01 23:48:45 +0000

    Add stylelint.

--- scripts/common/reporter.js
+++ scripts/common/reporter.js
@@ -4,9 +4,20 @@ const chalk = require('chalk');
 
 let nrSteps = 0;
 
+function renderLabel(label, color) {
+    let str = '\n' + chalk.inverse[color](' ' + label + ' ') + '\n';
+
+    for (let x = 0; x < 8; x += 1) {
+        str += chalk[color]('\u2594\u2594\u2594\u2594\u2594\u2594\u2594\u2594\u2594\u2594');
+    }
+
+    str += '\n';
+
+    return str;
+}
+
 function fail(err, extraMsg) {
-    process.stderr.write('\n');
-    process.stderr.write(chalk.underline.bold.red('ERROR') + ':\n');
+    process.stderr.write(renderLabel('ERROR', 'red'));
     process.stderr.write(err.message + '\n');
 
     if (extraMsg) {
@@ -18,12 +29,11 @@ function fail(err, extraMsg) {
 }
 
 function fatal(err) {
-    process.stderr.write('\n');
-    process.stderr.write(chalk.underline.bold.red('ERROR') + ':\n');
+    process.stderr.write(renderLabel('FATAL', 'red'));
     process.stderr.write(err.message + '\n');
 
     process.stderr.write('\n');
-    process.stderr.write(chalk.underline.bold.white('STACK') + ':\n');
+    process.stderr.write(renderLabel('STACK', 'yellow'));
     process.stderr.write(err.stack + '\n');
 
     process.exit(1);
@@ -31,12 +41,11 @@ function fatal(err) {
 
 function step(msg) {
     nrSteps += 1;
-    process.stdout.write(chalk.cyan(nrSteps + '.') + ' ' + msg + '\n');
+    process.stdout.write(renderLabel(nrSteps + '. ' + msg, 'white'));
 }
 
 process.on('uncaughtException', fatal);
 process.on('unhandledRejection', fatal);
-
 
 module.exports = {
     step,
